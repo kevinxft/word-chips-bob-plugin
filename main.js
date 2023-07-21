6c8c57fd344958472f8b03a2da730404f8dec21c
MR.Kevin <kevinxft@gmail.com>
2023-07-21 15:30:57 +0800

    调整接口返回信息

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-function getToken() {
-  const token = $option.token.trim();
-  return token;
-}
+var ENV_URL = {
+  dev: "http://localhost:3000",
+  test: "http://192.168.2.88:3000",
+  pro: "",
+};
 
 function isAllAlphabetWithSpace(str) {
   return /^[a-zA-Z\s]+$/.test(str);
@@ -15,23 +16,6 @@ function supportLanguages() {
   return ["auto", "zh-Hans", "en"];
 }
 
-$log.info("word-chips plugin");
-
-async function translate(query) {
-  const token = getToken();
-  if (!token) {
-    done(query, "请先设置Token");
-  } else {
-    const word = query.text.trim();
-    if (isSingleWord(word) && isAllAlphabet(word)) {
-      await addWord(word);
-      done(query, `单词 ${word} 添加成功`);
-    } else {
-      done(query, "目前仅支持单个单词");
-    }
-  }
-}
-
 function done(query, text) {
   query.onCompletion({
     result: {
@@ -41,23 +25,44 @@ function done(query, text) {
   $log.info(text);
 }
 
+$log.info("word-chips plugin");
+
+function callback(query) {
+  return function (resp) {
+    const { message } = resp.data;
+    done(query, message);
+  };
+}
+
+async function translate(query) {
+  if (!$option.token.trim()) {
+    done(query, "请先设置Token");
+  } else {
+    const word = query.text.trim();
+    if (isSingleWord(word) && isAllAlphabet(word)) {
+      await addWord(word, callback(query));
+    } else {
+      done(query, "目前仅支持单个单词");
+    }
+  }
+}
+
 function isSingleWord(text) {
   const arr = text.split(" ");
-  $log.info(arr.length);
   return arr.length === 1;
 }
 
-async function addWord(word) {
-  const token = getToken();
+async function addWord(word, handler) {
   await $http.request({
     method: "POST",
-    url: "http://192.168.2.105:3000/userBook/addWordFromOthers",
+    url: `${ENV_URL[$option.env]}/userBook/addWordFromOthers`,
     header: {
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${$option.token.trim()}`,
       "Content-Type": "application/json",
     },
     body: {
       name: word,
     },
+    handler,
   });
 }
